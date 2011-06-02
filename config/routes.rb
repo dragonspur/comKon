29f2c5dddc227e636834e94336cd38cb3deab282
James Xu <james_k_xu@yahoo.ca>
2011-06-01 23:45:59 -0700

    partial finish authtentication with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ComKon::Application.routes.draw do
+  devise_for :users
+
+  get "pages/home"
+
+  get "pages/contact"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ ComKon::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
