95c3fdc4fd2039efe4044d473a62f82e7f751663
James <james_k_xu@yahoo.ca>
2011-06-03 18:15:41 -0700

    added public post to first home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+	if user_signed_in?
+		@posts=Post.all
+	else 
+		@posts=Post.pub
+	end
   end
 
   def contact
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
-	attr_accessible :accessmode,:title,:content
+	attr_accessible :accessmode,:title,:content,:updated_at
+	scope :pub, :conditions => {:accessmode =>1}
+	scope :priv, :conditions => {:accessmode =>0}
 end
